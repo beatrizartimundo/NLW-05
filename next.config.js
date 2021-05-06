4c686c3e13c1dd7b60894ce56b6ad70a86c1579a
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-05-06 18:42:13 -0300

    add db

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const withPWA = require("next-pwa");
 
-module.exports ={
-    images:{
-        domains:['storage.googleapis.com']
-    }
-}
+module.exports = withPWA({
+  future: {webpack5: true},
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
+
+module.exports = {
+  images: {
+    domains: ['storage.googleapis.com'],
+  }
+}; 
