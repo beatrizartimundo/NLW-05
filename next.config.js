295d708c0390ba277db65b4ef01301170c546558
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-05-08 17:08:37 -0300

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  future: {webpack5: true},
+  future: {
+    webpack5: true
+  },
   pwa: {
     dest: "public",
-    register: true,
-    skipWaiting: true,
+    // register: true,
+    // skipWaiting: true,
+    // scope:'default',
+    // swSrc:'service-worker.js',
   },
-});
-
-module.exports = {
   images: {
     domains: ['storage.googleapis.com'],
   }
-}; 
+});
+
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,23 @@
- import Document,{Html,Head,Main,NextScript} from 'next/document'
+ import Document,{Html,Head,Main,NextScript, DocumentContext} from 'next/document'
 
  export default class MyDocument extends Document{
+    static async getInitialProps(ctx: DocumentContext) {
+        return await Document.getInitialProps(ctx);
+      }
      render(){
          return(
              <Html>
                  <Head>
+                    <meta name='application-name' content="name" />
+                    <meta name='apple-mobile-web-app-capable' content='yes' />
+                    <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+                    <meta name='apple-mobile-web-app-title' content="name"  />
+                    <meta name='description' content="name"  />
+                    <meta name='format-detection' content='telephone=no' />
+                    <meta name='mobile-web-app-capable' content='yes' />
+                    <meta name="theme-color" content="#FFFFFF"/>
+                    <link rel="apple-touch-icon" sizes='180x180'href="/icon.png"></link>
+                    <link rel="manifest" href="/manifest.json"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
                     <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
