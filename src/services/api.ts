434ed3d63e10a25f3f2c20ab416da8dea4ca105f
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-05-09 18:48:19 -0300

    config enviroments

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+const API_ENVS = {
+    production: 'https://my-json-server.typicode.com/beatrizartimundo/NLW-05/',
+    local: 'http://localhost:3333'
+  }
+
 export const api = axios.create({
     // baseURL: 'http://localhost:3333/'
-    baseURL: 'http://localhost:3333/'
+    baseURL: API_ENVS[process.env.NODE_ENV] || API_ENVS.local
 })
